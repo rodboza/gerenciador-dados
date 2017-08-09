688520e98a7a4458af3772157760b443c0bc6339
Rodrigo Barboza <rodboza@gmail.com>
2017-08-09 05:27:16 -0300

    ajustando mongoose

--- app/modelo/dados-climaticos/dados-coletor.js
+++ app/modelo/dados-climaticos/dados-coletor.js
@@ -1,9 +1,7 @@
-const restful = require('node-restful');
-const mongoose = require('mongoose');
 
 module.exports = function(app){
 
-  const dadosColetorSchema = new mongoose.Schema({
+  const dadosColetorSchema = new app.mongoose.Schema({
       ocorrencia: { type: Date, required: true },
       temperatura: { type: Number },
       umidade: { type: Number },
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const allowCors = require('./cors');
 const queryParser = require('express-query-int');
 const consign = require('consign');
-require('./mongoose');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +14,7 @@ app.use(allowCors);
 app.use(queryParser());
 
 app.env = require('./.env');
+app.mongoose = require('./mongoose');
 
 consign({cwd: 'app'})
   .include('database')
