1d3180df4cf5072f11968724a714bcf2564720a5
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 15:30:23 -0300

    Update erros.js

--- config/erros.js
+++ config/erros.js
@@ -2,10 +2,13 @@
 
 const _  = require('lodash');
 
-const sendErrorsFromDB = (res, dbErrors) => {
-  const errors = [];
-  _.forIn(dbErrors.errors, error => errors.push(error.message));
-  return res.status(400).json({ errors });
+const sendErrorsFromDB = (res, dbErrors, next) => {
+  if dbErrors {
+    const errors = [];
+    _.forIn(dbErrors.errors, error => errors.push(error.message));
+    return res.status(400).json({ errors });
+  }
+  next();
 }
 
 module.exports = sendErrorsFromDB;
