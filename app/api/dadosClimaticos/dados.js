abee9f21821fef80ff624800103addd0df42f5b9
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 15:45:02 -0300

    Update dados.js

--- app/api/dadosClimaticos/dados.js
+++ app/api/dadosClimaticos/dados.js
@@ -3,11 +3,11 @@
 module.exports = function(app) {
 
   var api = {};
-  Dados = app.database.dbDadosClimaticos.model('Dados');
+  dadosModelo = app.modelo.dadosClimatico.dados
 
   api.getDadosAtual = function(req, res, next) {
 
-    Dados.findOne({$query:{}, $orderby:{ocorrencia:-1}} , (err, dados) => {
+    dadosModelo.findOne({$query:{}, $orderby:{ocorrencia:-1}} , (err, dados) => {
       if (err) {
         return app.sendErrorsFromDB(res, err);
       } else if (dados) {
@@ -21,7 +21,7 @@ module.exports = function(app) {
 
     api.getDados = function(req, res, next) {
 
-      Dados.find({} , (err, dados) => {
+      dadosModelo.find({} , (err, dados) => {
         if (err) {
           return app.sendErrorsFromDB(res, err);
         } else if (dados) {
@@ -37,7 +37,7 @@ module.exports = function(app) {
     const temperatura = req.body.temperatura || -999
     const umidadeumidade = req.body.umidade || -999
     const pressao = req.body.pressao || -999
-    const newDados = new Dados({ ocorrencia, temperatura, umidade, pressao });
+    const newDados = new dadosModelo({ ocorrencia, temperatura, umidade, pressao });
     //const newDados = new Dados({ ocorrencia: Date.now(), temperatura: i, umidade: i, pressao:i });
     newDados.save()
     .then(
