59254009edebb3ffa9a0697042a6b23269552964
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 09:41:05 -0300

    add node-restful

--- app/database/dbDadosClimaticos.js
+++ app/database/dbDadosClimaticos.js
@@ -1,11 +1,10 @@
 
-const mongoose = require('mongoose');
 
 module.exports = function(app) {
 
   var db = {};
 
-  db = mongoose.createConnection(app.env.connectString);
+  db = app.mongoose.createConnection(app.env.connectString);
 
   return db;
 
