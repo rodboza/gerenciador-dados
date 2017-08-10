8bf0ab5708da26295a4a1fb10584d6f7fbf24173
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 15:32:54 -0300

    Update erros.js

--- config/erros.js
+++ config/erros.js
@@ -3,7 +3,7 @@
 const _  = require('lodash');
 
 const sendErrorsFromDB = (res, dbErrors, next) => {
-  if dbErrors {
+  if (dbErrors) {
     const errors = [];
     _.forIn(dbErrors.errors, error => errors.push(error.message));
     return res.status(400).json({ errors });
