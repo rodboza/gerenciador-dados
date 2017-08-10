15cf22d106b2e5de2664fff5b299a314cf0185c7
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 15:31:45 -0300

    Update configuracao.js

--- app/rota/dadosClimatico/configuracao.js
+++ app/rota/dadosClimatico/configuracao.js
@@ -6,7 +6,15 @@ module.exports = function(app) {
   console.log(configuracaoModel);
   
   configuracaoModel.methods(['get', 'post', 'put', 'delete']);
+  
+  configuracaoModel
+    .after('post', app.sendErrorsFromDB)
+    .after('put', app.sendErrorsFromDB)
+    .after('get', app.sendErrorsFromDB)
+    .after('delete', app.sendErrorsFromDB);
+  
   configuracaoModel.updateOptions({new: true, runValidators: true});
+  
   configuracaoModel.register(app,'/dadosClimatico/configuracao');
   
   var api = app.api.dadosClimaticos.configuracao;
