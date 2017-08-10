861c4d25427ed1e87a584fae299d8fdaa40da86f
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-10 15:49:02 -0300

    Update dados.js

--- app/rota/dadosClimatico/dados.js
+++ app/rota/dadosClimatico/dados.js
@@ -3,11 +3,25 @@
 module.exports = function(app) {
 
 
-  var api = app.api.dadosClimaticos.dados;
+  var dadosModelo = app.modelo.dadosClimatico.dados;
 
-  app.get("/dadosClimatico/dados", api.getDados);
+  dadosModelo
+    .after('post', app.sendErrorsFromDB)
+    .after('put', app.sendErrorsFromDB)
+    .after('get', app.sendErrorsFromDB)
+    .after('delete', app.sendErrorsFromDB);
+  
+  dadosModelo.updateOptions({new: true, runValidators: true});
+  
+  dadosModelo.register(app,'/dadosClimatico/dados');
+  
+  
+  var api = app.api.dadosClimaticos.dados;
+  
+  
+ // app.get("/dadosClimatico/dados", api.getDados);
   app.get("/dadosClimatico/dados/atual", api.getDadosAtual);
-  app.post("/dadosClimatico/dados", api.registrarDados);
+//  app.post("/dadosClimatico/dados", api.registrarDados);
 
 
 };
