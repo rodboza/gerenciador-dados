ca37325df8d11ac874c70faeffe25dd44add3e5d
Rodrigo Barboza <rodboza@gmail.com>
2017-08-15 10:17:53 -0300

    base

--- app/database/dbDadosClimaticos.js
+++ app/database/dbDadosClimaticos.js
@@ -2,14 +2,9 @@ console.log("inicio dbDadosClimaticos");
 
 
 module.exports = function(app) {
-
   var db = {};
-  //var mongo = new app.mongoose();
-  db = app.mongoose.connect(app.env.connectStringdbClimatico, {useMongoClient:true});
-
-
+  db = app.mongoose.connect(app.env.connectString.dbClimatico, {useMongoClient:true});
   return db;
-
 }
 
 console.log("fim dbDadosClimaticos");
--- app/database/dbOutroApp.js
+++ app/database/dbOutroApp.js
@@ -1,20 +1,9 @@
 console.log("inicio dbAuth");
 
-const mongoose = require('mongoose');
-
 module.exports = function(app) {
-
   var db = {};
-  //var mongo = new app.mongoose();
-  //db = mongo.connect(app.env.connectStringdbAuth, {useMongoClient:true});
-
-  //mongo.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
-//  mongo.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'.";
-  //mongo.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'.";
-//  mongo.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'.";
-
+  db = app.mongoose.connect(app.env.connectString.dbAuth, {useMongoClient:true});
   return db;
-
 }
 
 console.log("fim dbAuth");
