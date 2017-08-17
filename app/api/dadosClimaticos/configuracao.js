10192316bd05a5272e00c0707a2998e205b879c7
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-17 11:56:13 -0300

    Update configuracao.js

--- app/api/dadosClimaticos/configuracao.js
+++ app/api/dadosClimaticos/configuracao.js
@@ -5,20 +5,50 @@ module.exports = function(app) {
   var api = {};
   configuracaoModelo = app.modelo.dadosClimatico.configuracao;
 
+  //INICIO FUNCAO
   api.getIntervalo = function(req, res, next) {
 
     configuracaoModelo.findOne({} , (err, configuracao) => {
-      if (err) {
+      
+      if (err)
         return app.sendErrorsFromDB(res, err);
-      } else if (configuracao) {
+      
+      if (configuracao) {
         const intervalo = configuracao.intervalo;
         return res.status(200).json(intervalo);
-      } else {
-        return res.sendStatus(204);
-      }
+      } 
+      
+      return res.sendStatus(204);
+      
     })
   }
+  //FIM FUNCAO
 
+  //INICIO FUNCAO
+  api.getAll = function(req, res, next) {
+  }
+  //FIM FUNCAO
+
+  //INICIO FUNCAO
+  api.getOne = function(req, res, next) {
+  }
+  //FIM FUNCAO
+
+  //INICIO FUNCAO
+  api.post = function(req, res, next) {
+  }
+  //FIM FUNCAO
+
+  //INICIO FUNCAO
+  api.put = function(req, res, next) {
+  }
+  //FIM FUNCAO
+
+  //INICIO FUNCAO
+  api.put = function(req, res, next) {
+  }
+  //FIM FUNCAO
 
   return api;
+  
 };
