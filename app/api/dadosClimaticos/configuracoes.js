9b604e2397b8ceab5c1d745652c561c52d8bccbc
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-18 10:36:40 -0300

    Update configuracoes.js

--- app/api/dadosClimaticos/configuracoes.js
+++ app/api/dadosClimaticos/configuracoes.js
@@ -7,47 +7,86 @@ module.exports = function(app) {
 
   //INICIO FUNCAO
   api.getNomeValor = function(req, res, next) {
-    var busca = req.params.nome;
-    configuracaoModelo.findOne( {nome:'INTERVALO'} , (err, configuracao) => {
-
+    configuracaoModelo.findOne( {nome:req.params.nome} , (err, configuracao) => {
       if (err)
         return app.sendErrorsFromDB(res, err);
+      if (!configuracao)
+        return res.sendStatus(404);
+      return res.status(200).json(configuracao.valor);
+    });
+  }
+  //FIM FUNCAO
 
-      if (configuracao)
-        return res.status(200).json(configuracao.valor);
-
-      return res.sendStatus(404);
-    })
+  //INICIO FUNCAO
+  api.getId = function(req, res, next) {
+    configuracaoModelo.findOne( {_id:req.params.id} , (err, configuracao) => {
+      if (err)
+        return app.sendErrorsFromDB(res, err);
+      if (!configuracao)
+        return res.sendStatus(404);
+      req.configuracao = configuracao;
+      next();
+    });
   }
   //FIM FUNCAO
 
   //INICIO FUNCAO
   api.getAll = function(req, res, next) {
-    return res.sendStatus(404);
+    configuracaoModelo.find({}, (err, configuracao) => {
+      if (err)
+        return app.sendErrorsFromDB(res, err);
+      if (!configuracao)
+        return res.sendStatus(404);
+      return res.status(200).json(configuracao);
+    });
   }
   //FIM FUNCAO
 
   //INICIO FUNCAO
   api.getOne = function(req, res, next) {
-    return res.sendStatus(404);
+    return res.status(200).json(req.configuracao);
   }
   //FIM FUNCAO
 
   //INICIO FUNCAO
   api.post = function(req, res, next) {
-    return res.sendStatus(404);
+    var config = new configuracaoModelo();
+    config.nome = req.body.nome;
+    config.valor = req.body.valor;
+    config.save(
+      (err) => {
+        if (err) 
+          return app.sendErrorsFromDB(res, err);
+      }
+    );
+    return res.status(201).json(config);
   }
   //FIM FUNCAO
 
   //INICIO FUNCAO
   api.put = function(req, res, next) {
-    return res.sendStatus(404);
+    var config = req.configuracao;
+    config.valor = req.body.valor;
+    config.save(
+      (err) => {
+        if (err) 
+          return app.sendErrorsFromDB(res, err);
+      }
+    );
+    return res.status(201).json(config);
   }
   //FIM FUNCAO
 
   //INICIO FUNCAO
   api.delete = function(req, res, next) {
-    return res.sendStatus(404);
+    configuracaoModelo.remove(
+        {_id:req.configuracao._id}, 
+        (err, bear) => {
+            if (err)
+              return app.sendErrorsFromDB(res, err);
+        }
+    );
+    return res.sendStatus(200);
   }
   //FIM FUNCAO
 
