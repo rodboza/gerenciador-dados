ff9a6f178458753f122977fdaac5927ace9399a5
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-08-29 14:48:03 -0300

    Update dados.js

--- app/api/dadosClimaticos/dados.js
+++ app/api/dadosClimaticos/dados.js
@@ -22,6 +22,7 @@ module.exports = function(app) {
   //INICIO FUNCAO
   api.getId = function(req, res, next) {
     var obj = new require('Mongoose').Types.ObjectId('59a4539865011629048b3eb4');
+    console.log(obj);
     configuracaoModelo.find({})
       .where('_id').eq(obj)
       .exec()
